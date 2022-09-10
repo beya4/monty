f935d1b1bf01afd76d4a19752f64b4534ff4caf4
behailu <behailuis4simachew@gmail.com>
2022-09-09 17:03:30 -0700

    Added all

---
+++ test/main2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+
+
+int main(void)
+{
+int arr[2], num;
+char s[] = "Home ", *ptr, *pt;
+
+ptr = strtok(s, " ");
+pt = strtok(NULL, " ");
+num = atoi(pt);
+
+
+printf("%s\n%s\n", ptr, pt);
+return (0);
+}
